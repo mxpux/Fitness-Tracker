f549b13169d62dcc602475586820ddbd0a7f39a9
MPx <hellomariap@gmail.com>
2021-08-14 14:19:40 -0700

    Added starter files, and models index.js

---
+++ models/index.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: () => new Date(),
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Enter a type"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Enter a name"
+            },
+            duration: {
+                type: Number,
+                required: "Enter a duration"
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            },
+
+        }
+    ],
+
+})
+const Workout = mongoose.model("Workout", WorkoutSchema);
+module.exports = Workout;
